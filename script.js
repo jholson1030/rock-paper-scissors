22b6ea37110ce6b915c8ca12df842aaf2b141210
John Olson <jholson1030@gmail.com>
2022-08-12 16:59:04 -0500

    Add playRound function

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-const choices = ["rock", "paper", "scissors"];
+const choices = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
-console.log(getComputerChoice());
+
+function playRound() {
+    
+    const playerSelection = prompt("Rock, Paper, or Scissors?");
+    const computerSelection = getComputerChoice();
+
+    if (computerSelection == playerSelection) {
+        console.log("It's a tie.");
+    } else if (computerSelection == "Rock" && playerSelection == "Paper") {
+        console.log("Paper covers rock. You win!");
+    } else if (computerSelection == "Rock" && playerSelection == "Scissors") {
+        console.log("Rock crushes scissors. You lose...");
+    } else if (computerSelection == "Scissors" && playerSelection == "Paper") {
+        console.log("Scissors cuts paper. You lose...");
+    } else if (computerSelection == "Scissors" && playerSelection == "Rock") {
+        console.log("Rock crushes scissors. You win!");
+    } else if (computerSelection == "Paper" && playerSelection == "Rock") {
+        console.log("Paper covers rock. You lose...");
+    } else console.log("Scissors cuts paper. You win!");
+    
+
+} playRound();
