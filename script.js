4447ca3c28fac4109713db5cc85fbb214fb579bc
John Olson <jholson1030@gmail.com>
2022-09-22 16:09:54 -0500

    Put click events on buttons with possible outcomes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const startGame = 
+
 
 const choices = ["rock", "paper", "scissors"];
 var playerScore = 0;
@@ -11,56 +11,98 @@ function getComputerChoice() {
 }
 
 
-function playRound() {
+// Turn these into functions with possible outcomes (2 each)
+document.getElementById('rock').addEventListener('click', function () {
+    const computerSelection = getComputerChoice();
+    if (computerSelection == 'paper') {
+        computerScore++;
+        document.querySelector('.result').textContent = "You lose, paper covers rock.";
+        document.querySelector('computer-score').textContent = `Computer's score is: ${computerScore}`;
+    } else if (computerSelection == 'scissors') {
+        playerScore++;
+        document.querySelector('.result').textContent = "You win, rock crushes scissors.";
+        document.querySelector('player-score').textContent = `Your score is: ${playerScore}`;
+    } else document.querySelector('.result').textContent = "It's a tie, go again.";
+});
+
+document.getElementById('paper').addEventListener('click', function () {
+    const computerSelection = getComputerChoice();
+    if (computerSelection == 'rock') {
+        playerScore++;
+        document.querySelector('.result').textContent = "You win, paper covers rock.";
+        document.querySelector('player-score').textContent = `Your score is: ${playerScore}`;
+    } else if (computerSelection == 'scissors') {
+        computerScore++;
+        document.querySelector('.result').textContent = "You lose, scissors cuts paper.";
+        document.querySelector('computer-score').textContent = `Computer's score is: ${computerScore}`;
+    } else document.querySelector('.result').textContent = "It's a tie, go again.";
+});
+
+document.getElementById('scissors').addEventListener('click', function () {
+    const computerSelection = getComputerChoice();
+    if (computerSelection == 'rock') {
+        computerScore++;
+        document.querySelector('.result').textContent = "You lose, paper covers rock.";
+        document.querySelector('computer-score').textContent = `Your score is: ${computerScore}`;
+    } else if (computerSelection == 'paper') {
+        playerScore++;
+        document.querySelector('.result').textContent = "You win, scissors cuts paper.";
+        document.querySelector('player-score').textContent = `Computer's score is: ${playerScore}`;
+    } else document.querySelector('.result').textContent = "It's a tie, go again.";
+});
+
+
+
+/* function playRound() {
     
-    const playerSelection = 
+    var playerSelection;
     const computerSelection = getComputerChoice();
 
     if (computerSelection == playerSelection) {
-        
-    } else if (computerSelection == "rock" && playerSelection.toLowerCase() == "paper") {
-        
+        document.querySelector('.result').textContent = "It's a tie.";
+    } else if (computerSelection == "rock" && playerSelection == playerPaper) {
+        document.querySelector('.result').textContent = "You win, paper covers rock.";
         playerScore++;
-        
-    } else if (computerSelection == "rock" && playerSelection.toLowerCase() == "scissors") {
-        
+        document.querySelector('player-score').textContent = `Your score is: ${playerScore}`;
+    } else if (computerSelection == "rock" && playerSelection == playerScissors) {
+        document.querySelector('.result').textContent = "You lose, rock crushes scissors.";
         computerScore++;
-        
-    } else if (computerSelection == "scissors" && playerSelection.toLowerCase() == "paper") {
-        
+        document.querySelector('computer-score').textContent = `Your score is: ${computerScore}`;
+    } else if (computerSelection == "scissors" && playerSelection == playerPaper) {
+        document.querySelector('.result').textContent = "You lose, scissors cuts paper.";
         computerScore++;
-        
-    } else if (computerSelection == "scissors" && playerSelection.toLowerCase() == "rock") {
-        
+        document.querySelector('computer-score').textContent = `Your score is: ${computerScore}`;
+    } else if (computerSelection == "scissors" && playerSelection == playerRock) {
+        document.querySelector('.result').textContent = "You win, rock crushes scissors.";
         playerScore++;
-        
-    } else if (computerSelection == "paper" && playerSelection.toLowerCase() == "rock") {
-        
+        document.querySelector('player-score').textContent = `Your score is: ${playerScore}`;
+    } else if (computerSelection == "paper" && playerSelection == playerRock) {
+        document.querySelector('.result').textContent = "You lose, paper covers rock.";
         computerScore++;
-        
-    } else if (computerSelection == "paper" && playerSelection.toLowerCase() == "scissors") {
-       
+        document.querySelector('computer-score').textContent = `Your score is: ${computerScore}`;
+    } else if (computerSelection == "paper" && playerSelection == playerScissors) {
+        document.querySelector('.result').textContent = "You win, scissors cuts paper.";
         playerScore++;
-        
+        document.querySelector('player-score').textContent = `Your score is: ${playerScore}`;
     }   
     
 
-}
+} */
 
 function game() {
+    document.querySelector('.start-game').style.display = "none";
     while (playerScore < 5 && computerScore < 5) {
         playRound();
     }
     if (playerScore == 5) {
-        
+        document.querySelector('.result').textContent = "You're the winner!!!";
+        document.querySelector('.reset-game').style.display = "flex";
     } else if (computerScore == 5) {
-        
+        document.querySelector('.result').textContent = "You are a total loser..";
+        document.querySelector('.reset-game').style.display = "flex";
     } 
 }
 
-function startGame() {
-    // Will begin the game
-}
 
 function resetGame() {
     // Will restart the game
