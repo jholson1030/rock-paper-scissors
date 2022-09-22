457da7bab0fa6e6c801eb2a836d70d98593fb164
John Olson <jholson1030@gmail.com>
2022-09-22 13:19:21 -0500

    removed alerts and create startGame()

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+const startGame = 
+
 const choices = ["rock", "paper", "scissors"];
 var playerScore = 0;
 var computerScore = 0;
 
+
+// Update the DOM with computer selection
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
 }
@@ -9,36 +13,36 @@ function getComputerChoice() {
 
 function playRound() {
     
-    const playerSelection = prompt("Rock, Paper, or Scissors?");
+    const playerSelection = 
     const computerSelection = getComputerChoice();
 
     if (computerSelection == playerSelection) {
-        return alert("It's a tie.");
+        
     } else if (computerSelection == "rock" && playerSelection.toLowerCase() == "paper") {
-        alert("Paper covers rock. You win!");
+        
         playerScore++;
-        return alert("Your Score: " + playerScore + "\nComputer Score: " + computerScore);
+        
     } else if (computerSelection == "rock" && playerSelection.toLowerCase() == "scissors") {
-        alert("Rock crushes scissors. You lose...");
+        
         computerScore++;
-        return alert("Your Score: " + playerScore + "\nComputer Score: " + computerScore);
+        
     } else if (computerSelection == "scissors" && playerSelection.toLowerCase() == "paper") {
-        alert("Scissors cuts paper. You lose...");
+        
         computerScore++;
-        return alert("Your Score: " + playerScore + "\nComputer Score: " + computerScore);
+        
     } else if (computerSelection == "scissors" && playerSelection.toLowerCase() == "rock") {
-        alert("Rock crushes scissors. You win!");
+        
         playerScore++;
-        return alert("Your Score: " + playerScore + "\nComputer Score: " + computerScore);
+        
     } else if (computerSelection == "paper" && playerSelection.toLowerCase() == "rock") {
-        alert("Paper covers rock. You lose...");
+        
         computerScore++;
-        return alert("Your Score: " + playerScore + "\nComputer Score: " + computerScore);
+        
     } else if (computerSelection == "paper" && playerSelection.toLowerCase() == "scissors") {
-        alert("Scissors cuts paper. You win!");
+       
         playerScore++;
-        return alert("Your Score: " + playerScore + "\nComputer Score: " + computerScore);
-    } else return alert("That is not a valid option. \nEnter 'rock, paper, or scissors'");    
+        
+    }   
     
 
 }
@@ -48,9 +52,16 @@ function game() {
         playRound();
     }
     if (playerScore == 5) {
-        return alert("You win! The final score is: \nYou: " + playerScore + "\nComputer: " + computerScore);
+        
     } else if (computerScore == 5) {
-        return alert("You lose... The final score is: \nYou: " + playerScore + "\nComputer: " + computerScore);
-    } else return alert("It's a tie...");    
+        
+    } 
 }
 
+function startGame() {
+    // Will begin the game
+}
+
+function resetGame() {
+    // Will restart the game
+}
